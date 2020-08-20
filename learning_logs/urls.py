2f256709581551480893ee69f70533afa295d6e2
mamtapandey1910 <loginmamta1910@gmail.com>
2020-08-21 01:38:35 +0530

    Topics has been added to individual pages

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -8,4 +8,10 @@ app_name = 'learning_logs'
 urlpatterns =[
     # Home page
     path('', views.index, name= 'index'),
+
+    # Page that shows all the topic
+    path('topic/', views.topics, name= 'topics'),
+
+    # Detail page for a single topic.
+    path('topic/<int:topic_id>/', views.topic, name= 'topic')
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from .models import Topic
 
 def index(request):
     """ The home page for Learning Log."""
     return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """ Show all the topics"""
+    topics= Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    """ show all the topics."""
+    topic= Topic.objects.get(id= topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return  render(request, 'learning_logs/topic.html', context)
+
